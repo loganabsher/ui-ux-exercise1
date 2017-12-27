4e0b9cfed1e329e634512f4b4f32e1bc220e9d6b
Logan Absher <absherlogan@gmail.com>
2017-12-27 14:35:47 -0800

    added event handlers to all buttons to change the state of the app, did some minor styling

--- data-table/src/component/userTableContainer/index.js
+++ data-table/src/component/userTableContainer/index.js
@@ -1,19 +1,30 @@
 'use strict';
 
+import './userTableContainer.scss';
+
 import React from 'react';
 import Faker from 'faker';
 
 import UserTable from '../userTable';
+
+const limitArr = [5, 10, 25, 50, 75, 100];
+const sortArr = ['First Name', 'Last Name', 'Country', 'Address', 'City', 'State', 'Zip', 'Phone'];
 
 class UserTableContainer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       loading: true,
+      limit: 5,
+      offset: 0,
+      sorting: 'First Name',
       data: []
     }
+
+    this.changeOffSet = this.changeOffSet.bind(this);
+    this.changeLimit = this.changeLimit.bind(this);
   }
-      
+
   componentDidMount(){
     for(let i = 1000; i > 0; i--){
       this.state.data.push({
@@ -29,16 +40,54 @@ class UserTableContainer extends React.Component{
     }
     this.setState({loading: false});
   }
-    
+
+  changeOffSet(e){
+    let offset = parseInt(this.state.offset);
+    let limit = parseInt(this.state.limit);
+    if(e.target.textContent === 'prev'){
+      if(offset - limit > 0){
+        this.setState({offset: offset - limit});
+      }else{
+        this.setState({offset: 0});
+      }
+    }else{
+      if(offset + limit < this.state.data.length - limit){
+        this.setState({offset: offset + limit});
+      }else{
+        this.setState({offset: this.state.data.length - limit});
+      }
+    }
+  }
+
+  changeLimit(e){
+    this.setState({limit: e.target.options[e.target.selectedIndex].value})
+  }
+
   render(){
+    let offset = parseInt(this.state.offset);
+    let limit = parseInt(this.state.limit);
     return(
       <div className="app">
-        <ul>
-          <li><h1>List of Awesome | </h1></li>
-          <li><p>items per page</p><option id="limit-sorter"></option></li>
-          <li><p>something of something</p><button>prev</button><button>next</button></li>
+        <ul id="sorting-items">
+          <li><h1>List of Awesome</h1></li>
+          <li><p>|</p></li>
+          <li><p>Sort by</p>
+            <select>
+              {sortArr.map((val) => {
+                return(<option value={val}>{val}</option>)
+              })}
+            </select>
+          </li>
+          <li><p>items per page</p>
+            <select onChange={this.changeLimit}>
+              {limitArr.map((ele) => {
+                return(<option value={ele}>{ele}</option>)
+              })}
+            </select>
+          </li>
+          <li><p>{`${offset} - ${offset + limit}`}</p><button onClick={this.changeOffSet}>prev</button><button onClick={this.changeOffSet}>next</button></li>
         </ul>
-        {this.state.loading ? <p>...Loading</p> : <UserTable dataArr={this.state.data} />}
+        {this.state.loading ? <p>...Loading</p> : <UserTable dataArr={this.state.data.slice(offset, (offset + limit))} />}
       </div>
     )
   }
